db00d547ab379447f1192f336de6530e46df0a2e
Alex Osborne <ato@meshy.org>
2013-06-21 23:30:18 +1000

    Add deprecation notice to the project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject lein-outdated "1.1.0-SNAPSHOT"
-  :description "Leiningen plugin that lists new published versions of dependencies"
+(defproject lein-outdated "1.0.3"
+  :description "Leiningen plugin that lists new published versions of dependencies. DEPRECATED: Use lein-ancient instead"
   :url "http://github.com/ato/lein-outdated"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
